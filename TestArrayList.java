b5b568366f57b4daf358b47f6956011551463d1a
U10316007 <theanswer0411@gmail.com>
2015-04-17 16:02:13 +0800

    Update TestArrayList.java

--- TestArrayList.java
+++ TestArrayList.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 
 public class TestArrayList{
 	public static void main (String[]args){
-		MyStack MyStack1 = new MyStack();
-		Scanner input = new Scanner(System.in);
+		//excute program
+		MyStack MyStack1 = new MyStack();//new a object
+		Scanner input = new Scanner(System.in);//new a scanner
 		
 		for(int i=1;i<=5;i++){
+			//a for loop to ask 5 number 
 			System.out.println("please enter five number");
 			String num= input.next();
 			MyStack1.push(num);
-			
+			//inhertance MyStack.java
 		}
 		for(int a=1;a<=5;a++){
+			//a for loop to use pop method
 			System.out.println(MyStack1.pop()+"\n");
 		}
 	 
